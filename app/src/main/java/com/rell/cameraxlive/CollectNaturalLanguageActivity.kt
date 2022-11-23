c5f48316a3c50cc847f75eabda3517639dc29631
juyoung1002 <98293196+juyoung1002@users.noreply.github.com>
2022-11-23 15:45:25 +0900

    이미지에서 자연어를 추출하는 액티비티 생성

---
+++ app/src/main/java/com/rell/cameraxlive/CollectNaturalLanguageActivity.kt
@@ -0,0 +1,11 @@
+package com.rell.cameraxlive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CollectNaturalLanguageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collect_natural_language)
+    }
+}
